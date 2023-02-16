b298ae535c95a0ba66ddc6a7d9d2812d64942e70
Charan <gcharankumar99@gmail.com>
2023-02-15 19:34:52 -0600

    vars file

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
-module "network" {
+module "vpc" {
   source = "github.com/CharanKumar93/tf-module-vpc"
   env = var.env
   default_vpc_id = var.default_vpc_id
 
   for_each = var.vpc
   cidr_block = each.value.cidr_block
+}
+
+module "subnets" {
+  source = "github.com/CharanKumar93/tf-module-vpc"
+  env = var.env
+  default_vpc_id = var.default_vpc_id
+
+  vpc_id = module.vpc.vpc_id
+
+  for_each = var.subnets
+  cidr_block = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  name = each.value.name
 }
