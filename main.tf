023cbad37004e526c3baa011a2dff8af4a101bfd
Charan <gcharankumar99@gmail.com>
2023-02-19 20:27:26 -0600

    vars file

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 }
 
 module "docdb" {
-  source = "https://github.com/CharanKumar93/tf-module-docdb"
+  source = "github.com/CharanKumar93/tf-module-docdb"
   env    = var.env
   for_each            = var.docdb
   subnet_ids          = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
